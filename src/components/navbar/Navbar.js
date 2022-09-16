91856a9a39a033e3a0020ec4693c9f5e309bb310
Halil Can Cengiz <hllcncngz1@gmail.com>
2022-09-16 22:36:12 +0300

    changed route of basket page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -67,7 +67,7 @@ const Navbar = () => {
                             </ul>
                         </div>
                         <button className='navlink my-auto'><i className="my-auto me-2 fa-solid fa-bell"></i>Notification</button>
-                        <NavLink className="navlink my-auto" to="/basket"><span id='basketCount' className="badge bg-white text-dark me-2">{currentBasket.length}</span>Basket</NavLink>
+                        <NavLink className="navlink my-auto" to="/profile/basket"><span id='basketCount' className="badge bg-white text-dark me-2">{currentBasket.length}</span>Basket</NavLink>
                     </div>
                 </div>
             </div>
