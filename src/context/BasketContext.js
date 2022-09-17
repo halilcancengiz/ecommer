db695daea7cbfa7c2700b59955def5b310c378a2
Halil Can Cengiz <hllcncngz1@gmail.com>
2022-09-18 00:11:21 +0300

    updated basket page content and style

--- src/context/BasketContext.js
+++ src/context/BasketContext.js
@@ -1,22 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { createContext, useContext, useState, useEffect } from 'react';
 import { useUserContext } from './UserContext';
-import { ref, set, onValue } from 'firebase/database';
+import { ref, set, onValue, update } from 'firebase/database';
 import { db } from '../Firebase';
-import { uuidv4 } from '@firebase/util';
+import { remove } from 'firebase/database';
+import alertify from 'alertifyjs';
+
 
 
 const BasketContext = createContext()
 
 export const BasketContextProvider = ({ children }) => {
     const { userInfo } = useUserContext()
-
     const [basket, setBasket] = useState()
     const [currentBasket, setCurrentBasket] = useState([])
-    const basketID = uuidv4()
+    const productCount = 1
 
     const addProductToBasket = async (product) => {
-        await set(ref(db, `/baskets/${userInfo.uid}/` + basketID), {
+        await set(ref(db, `/baskets/${userInfo.uid}/${product.id}`), {
             id: product.id,
             createdAt: product.createdAt,
             createdWho: product.createdWho,
@@ -25,11 +26,60 @@ export const BasketContextProvider = ({ children }) => {
             category: product.category,
             price: product.price,
             moneytype: product.moneytype,
+            quantity: product.quantity,
+            shippingtype: product.shippingtype,
             url: product.url,
-            addingToCardBy: userInfo.uid
+            addingToCardBy: userInfo.uid,
+            productCount
         });
     }
 
+    const increaseProductCount = async (product) => {
+        if (product.productCount < product.quantity) {
+            await update(ref(db, `/baskets/${userInfo.uid}/${product.id}`), {
+                productCount: product.productCount + 1
+            })
+        } else {
+            alertify.error(`Bu ürün için en fazla ${product.quantity} sipariş verebilirsiniz.`)
+        }
+
+    }
+    const decreaseProductCount = async (product) => {
+        if (product.productCount > 1) {
+            await update(ref(db, `/baskets/${userInfo.uid}/${product.id}`), {
+                productCount: product.productCount - 1
+            })
+        }
+
+        else {
+            alertify.confirm('Ürünü Kaldırmak İstediğinize Emin Misiniz ?',
+                function () {
+                    deleteProductFromBasket(product)
+                    alertify.success('Ürün Sepetten Kaldırıldı', 1.5)
+                },
+                function () {
+                    alertify.error('Ürün Kaldırma İşlemi İptal Edildi', 1.5)
+                });
+        }
+
+
+    }
+
+    const clearBasket = async () => {
+        try {
+            await remove(ref(db, `baskets/${userInfo.uid}`))
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    const deleteProductFromBasket = async (product) => {
+        try {
+            await remove(ref(db, `baskets/${userInfo.uid}/${product.id}`))
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     const getBasketFromDatabase = async () => {
         onValue(ref(db, `baskets/${userInfo.uid}`), (snapshot) => {
@@ -46,7 +96,11 @@ export const BasketContextProvider = ({ children }) => {
     const values = {
         addProductToBasket,
         getBasketFromDatabase,
-        currentBasket
+        currentBasket,
+        clearBasket,
+        deleteProductFromBasket,
+        increaseProductCount,
+        decreaseProductCount
     }
 
 
--- src/pages/basket/Basket.js
+++ src/pages/basket/Basket.js
@@ -1,63 +1,120 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import alertify from 'alertifyjs'
 import React, { memo, useEffect } from 'react'
 import { useBasketContext } from "../../context/BasketContext"
+import Loading from "../../components/loading/Loading"
 import "./basket.css"
 
 const Basket = () => {
-  const { getBasketFromDatabase, currentBasket } = useBasketContext()
+  const { getBasketFromDatabase, currentBasket, clearBasket, deleteProductFromBasket, increaseProductCount, decreaseProductCount } = useBasketContext()
 
   useEffect(() => {
     getBasketFromDatabase()
   }, [])
 
+  const handleDeleteAllBasket = (myproduct) => {
+    alertify.confirm('Sepetteki Tüm Ürünleri Silmek İstediğinize Emin misiniz?',
+      function () {
+        clearBasket()
+        alertify.success('Sepet Temizlendi', 1.5)
+      },
+      function () {
+        alertify.error('Sepet Temizleme İptal Edildi', 1.5)
+      });
+  }
+  const handleDeleteProductFromBasket = (product) => {
+    alertify.confirm('Ürünü Sepetten Kaldırmak İstediğinize Emin misiniz?',
+      function () {
+        deleteProductFromBasket(product)
+        alertify.success('Ürün Kaldırıldı.', 1.5)
+      },
+      function () {
+        alertify.error('Ürün Silme İşlemi iptal Edildi.', 1.5)
+      });
+  }
+  const handleDecreaseProductCount = (product) => {
+    decreaseProductCount(product)
+  }
+  const handleIncreaseProductCount = (product) => {
+    increaseProductCount(product)
+  }
   return (
-    <>
-      <div className='bg-transperent my-5 container row'>
-        <div className='d-flex align-items-center justify-content-between'>
-          <h6>Shopping Cart</h6>
-          <button className={`${currentBasket.length < 2 ? "d-none" : "btn btn-danger"}`}>Remove All</button>
+    <div className='h-100'>
+      <div className='bg-transperent mx-auto my-5 container row'>
+        <div id='shoppingbasketHeaderContainer' className='d-flex align-items-center justify-content-between bg-white p-2 mb-3 col-12'>
+          <h6 className='fs-5 fw-bold p-0 m-0 ms-2' >Shopping Basket</h6>
+          <button onClick={handleDeleteAllBasket} className={`${currentBasket.length < 2 ? "d-none" : "d-block me-2 text-danger"}`}>Remove All</button>
         </div>
-        {
-          currentBasket && currentBasket.map(product => (
-            <div key={product.id} className='basketProductContainer d-flex align-items-center justify-content-center flex-row bg-white my-2 text-dark border'>
+        <div className='col-12'>
+          {currentBasket.length < 0 ? <Loading /> : currentBasket.map(product => (
+            <div className="container w-100 p-0">
+              <div className="basketProductContainer row mt-3 bg-white ">
 
-              <div className='basketProductImageContainer col-lg-3 h-100 d-flex align-items-center justify-content-center'>
-                <img className='h-75' src={product.url} alt={product.title} />
-              </div>
-
-              <div className='basketProductTitleContainer flex-column d-flex align-items-center justify-content-center col-lg-4 h-100'>
-                <div className='basketProductTitle d-flex align-items-center justify-content-center fs-4 fw-bold'>
-                  {product.title}
+                <div className="col-lg-3 col-sm-6 col-xs-10 d-flex align-items-center justify-content-center">
+                  <div className='basketProductImageContainer w-75 h-100'>
+                    <img className='w-100 h-100 p-2' src={product.url} alt={product.title} />
+                  </div>
                 </div>
-                <div className='freeShipping d-flex align-items-center justify-content-center flex-row border py-1 px-2 border-dark rounded-pill'>
-                  <i className="fa-solid fa-truck"></i>
-                  <span className='ms-2'>Ücretsiz Kargo</span>
+
+                <div className="basketProductTitleContainer col-lg-3 col-sm-6 col-xs-10 d-flex align-items-center justify-content-center flex-column">
+
+                  <div className='basketProductTitle d-flex align-items-center justify-content-center fs-4 fw-bold'>
+                    {product.title}
+                  </div>
+                  <div className='freeShipping d-flex align-items-center justify-content-center flex-row'>
+                    {
+                      product.shippingtype === "fast" ? (
+                        <div className='bg-primary text-white rounded-pill px-2 py-1'>
+                          <i className="fa-solid fa-truck-fast"></i>
+                          <span className='ms-2'>Hızlı Kargo</span>
+                        </div>
+                      ) : product.shippingtype === "free" ? (
+                        <div className='bg-dark text-white rounded-pill px-2 py-1'>
+                          <i className="fa-solid fa-truck"></i>
+                          <span className='ms-2'>Ücretsiz Kargo</span>
+                        </div>
+                      ) : (
+                        <div className='bg-danger text-white rounded-pill px-2 py-1'>
+                          <i className="fa-solid fa-circle-xmark"></i>
+                          <span className='ms-2'>Ücretli Kargo</span>
+                        </div>
+                      )
+                    }
+                  </div>
+
+                </div>
+
+                <div className="col-lg-3 col-sm-6 col-xs-10 d-flex align-items-center justify-content-center">
+                  <div className='basketProductQuantity d-flex align-items-center justify-content-center fs-4'>
+
+                    <button onClick={() => handleDecreaseProductCount(product)} className='bg-dark text-white d-flex align-items-center justify-content-center rounded-circle'>-</button>
+                    <div className='badge badge-white bg-white text-dark mx-3'>{product.productCount}</div>
+                    <button onClick={() => handleIncreaseProductCount(product)} className='bg-dark text-white d-flex align-items-center justify-content-center rounded-circle'>+</button>
+
+                  </div>
+                </div>
+
+                <div className="col-lg-3 col-sm-6 col-xs-10 d-flex align-items-center justify-content-center">
+                  <div className='basketProductInfoContainer d-flex align-items-center justify-content-center flex-column col-lg-3 col-md-6 h-100 fs-5'>
+                    <div className="basketProductPrice fw-bold">
+                      {`${product.price} ${product.moneytype}`}
+                    </div>
+                    <button onClick={() => handleDeleteProductFromBasket(product)} className='basketProductRemoveIcon btn btn-transperent text-danger border-0 '>
+                      Remove
+                    </button>
+                  </div>
                 </div>
               </div>
-
-              <div className='basketProductQuantity d-flex align-items-center justify-content-center col-lg-2 h-100 fs-4'>
-                <div className='px-2 bg-white text-dark rounded-circle'>-</div>
-                <div className='badge badge-white bg-white text-dark mx-3'>
-                  0
-                </div>
-                <div className='px-2 bg-white text-dark rounded-circle'>+</div>
-              </div>
-
-              <div className='basketProductInfoContainer d-flex align-items-center justify-content-center flex-column col-lg-3 h-100 fs-5'>
-                <div className="basketProductPrice">
-                  {`${product.price} ${product.moneytype}`}
-                </div>
-                <button className='basketProductRemoveIcon btn btn-transperent text-danger border-0 '>
-                  Remove
-                </button>
-              </div>
-
             </div>
           ))
-        }
+
+          }
+
+        </div>
+
 
       </div>
-    </>
+    </div>
   )
 }
 // {
@@ -66,3 +123,16 @@ const Basket = () => {
 //   ))
 // }
 export default memo(Basket)
+
+
+
+//
+
+
+
+
+        
+//       </div>
+//     </div>
+//   ))
+// }
